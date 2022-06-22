e4dbb04546da3177eb1bc4a91b3143a9b341e246
Aldo Martinez <aldo.martinez@u-payments.com>
2022-06-22 19:51:14 -0400

    Desafio uno card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,25 @@
-const Card = (props) => {
+const Card = ({name, min, max, img, onClose}) => {
   // acá va tu código
-  return <div>Card Component</div>;
+  return <div>
+    <button onClick={onClose} > X </button>
+    <h4>
+      {name}
+    </h4>
+    <p>
+      min
+    </p>
+    <p>
+      {min}
+    </p>
+    <p>
+      {max}
+    </p>
+    <p>
+      {max}
+    </p>
+    
+    <img  src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="imagen"/>
+  </div>;
 };
 
 export default Card;
